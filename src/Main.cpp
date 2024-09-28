14654ea1960d563007aeec582b363d1c33b8138e
Styyx <styyx228@gmail.com>
2024-09-28 10:13:46 +0200

    init template

---
+++ src/Main.cpp
@@ -0,0 +1,35 @@
+#include "Events.h"
+#include "Hooks.h"
+#include "Logging.h"
+#include "Settings.h"
+
+void Listener(SKSE::MessagingInterface::Message* message) noexcept
+{
+    if (message->type == SKSE::MessagingInterface::kDataLoaded) {
+        Settings::LoadSettings();
+        Events::EquipEventHandler::Register();
+        Hooks::Install();
+    }
+}
+
+SKSEPluginLoad(const SKSE::LoadInterface* skse)
+{
+    InitLogging();
+
+    const auto plugin{ SKSE::PluginDeclaration::GetSingleton() };
+    const auto name{ plugin->GetName() };
+    const auto version{ plugin->GetVersion() };
+
+    logger::info("{} {} is loading...", name, version);
+
+    Init(skse);
+
+    if (const auto messaging{ SKSE::GetMessagingInterface() }; !messaging->RegisterListener(Listener)) {
+        return false;
+    }
+
+    logger::info("{} has finished loading.", name);
+    logger::info("");
+
+    return true;
+}
